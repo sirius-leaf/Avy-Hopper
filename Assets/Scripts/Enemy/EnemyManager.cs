150696cfb334a07bfded6c1d9f3939b8d77d9545
Sirius Leaf <hfzamdhai@gmail.com>
2026-06-07 09:14:56 +0700

    tambah enemy manager

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject[] enemies = new GameObject[3];
+
+    private UiManager _ui;
+
+    void Awake()
+    {
+        _ui = Object.FindFirstObjectByType<UiManager>();
+
+        int enemyPos = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            if (enemies[i] == null) continue;
+
+            GameObject enemy = Instantiate(enemies[i], new Vector3(enemyPos * 3f, 0f, 0f), Quaternion.identity, transform);
+            _ui.enemyHealth[i] = enemy.GetComponent<EnemyHealth>();
+            
+            enemyPos++;
+        }
+
+        transform.position = new Vector3((2 - (transform.childCount - 1)) * 1.5f, -2f, 0f);
+    }
+}
